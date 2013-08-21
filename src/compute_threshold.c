da93e1b8ef32211cb103a051e73bfe3d6b4a9e5c
landais <landais@0f4c2047-2b94-43bf-a5af-26cc34c4479a>
2013-08-21 11:11:42 +0000

    ajout de compute_threshold.c

---
+++ src/compute_threshold.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libisd.h"
+
+double combin_d(int n, int k){
+	double x, y, i;
+	if(k > n){
+		return 0;
+	}
+	if (k > n/2){
+		k = n-k;
+	}
+	x = 1;
+	y = 1;
+	i = n-k+1;
+	while (i <= n){
+		x = (x*i)/y;
+		y += 1;
+		i += 1;
+	}
+	return x;
+}
+
+int main()
+{
+	FILE* f = stdin;
+	int i, T;
+	int k=0, r=0, w=0, p=0, l=0, a=0;
+	unsigned long long K0, K1, K2;
+	double p_false_alarm = 0;
+	double p_not_miss = 0;
+	char input[512];
+	double cost;
+	unsigned long long min_cost = 0;
+	int arg_min = -1;
+	int sucess = 0;
+	do {
+		if(fgets(input, 512, f) == NULL) {
+			break;
+		}
+	} while ((sucess = sscanf(input, "couts pour k=%d r=%d w=%d p=%d l=%d : %lld %lld %lld", &k, &r, &w, &p, &l, &K0, &K1, &K2)) != 8 && (!feof(f)));
+
+
+	int eff_word_bits = min(r, (int) word_len);
+	a = eff_word_bits - l;
+
+	if (sucess == 0) {
+		fprintf(stderr, "error : no data found\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("k=%d\nr=%d\nw=%d\np=%d\nl=%d\na=%d\nK0=%lld\nK1=%lld\nK2=%lld\n",k,r,w,p,l,a,K0,K1,K2);
+	printf("%2s : %12s %13s %40s\n", "T", "p_not_miss", "p_false_alarm", "cost");
+	for (T = 1; T < a; ++T) {
+		p_false_alarm = 0;
+		p_not_miss = 0;
+		for (i = 0; i < T; ++i) {
+			p_false_alarm += combin_d(a, i);
+		}
+		p_false_alarm /= (1ULL << a);
+
+		for (i = 0; i < T; ++i) {
+			p_not_miss += combin_d(a, i)*combin_d(r-l-a, w-p-i);
+		}
+		p_not_miss /= combin_d(r-l, w-p);
+
+		cost = (K0 + K1 + (p_false_alarm * K2 )) / p_not_miss;
+		printf("%2d : %12g %13g %40f\n", T, p_not_miss, p_false_alarm, cost);
+		if ((arg_min == -1) || (cost < min_cost)) {
+			arg_min = T;
+			min_cost = cost;
+		}
+	}
+	printf("\n");
+
+	if (K2 == 0) {
+		fprintf(stderr, "Final test cost was 0, this value could be wrong\n");
+	}
+	printf("%d\n", arg_min);
+	return 0;
+}
